c1252846d1d850cb45cc112815a045280233621c
RamboxEu <mail.rambox@gmail.com>
2019-02-08 18:31:59 +0100

    Change visibilty of main class

--- src/main/java/eu/rambox/fabritech/FabriTech.java
+++ src/main/java/eu/rambox/fabritech/FabriTech.java
@@ -2,7 +2,7 @@ package eu.rambox.fabritech;
 
 import net.fabricmc.api.ModInitializer;
 
-class FabriTech implements ModInitializer {
+public class FabriTech implements ModInitializer {
   @Override
   public void onInitialize() {
     System.out.print("Initializing FabriTech.");
